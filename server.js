93b282d4fe56e4b3143ae67cc3d4955ecc95ff3b
David Acosta <acosta816@gmail.com>
2019-08-05 15:22:09 -0700

    getting ready to deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-console.log(process.env.API_TOKEN);
+
 const app = express();
 const digimon = require('./database/digimon-database');
 
